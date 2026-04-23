c6e905e826f3203d9d2ea2a23c127f8717f827fd
ellathurai-dev <ellathurai1872004@gmail.com>
2026-04-23 08:27:16 +0530

    Add global exception handling

---
+++ exception handling/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.employee.management.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+}
